bd79d9c0d6500a5b405a2c08a7ba2de5a66e647b
Jason <jason@debian>
2021-12-03 21:45:13 +0700

    solution to life without zeros problem

---
+++ codeforces/life_without_zeros/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int removeZero(int n) {
+  int nWithoutZero = 0;
+  int digit = -1;
+  int count = 0;
+  
+  while (true) {
+    digit = n % 10;
+    n = n / 10;
+
+    if (digit != 0) {
+      nWithoutZero += digit * pow(10, count);
+      count++;
+    } 
+
+    if (n == 0) break;
+
+  }
+  return nWithoutZero;
+}
+
+int main () {
+  int a, b;
+  cin >> a >> b;
+  int c = a + b;
+  int aWithoutZero = removeZero(a);
+  int bWithoutZero = removeZero(b);
+  int cWithoutZero = aWithoutZero + bWithoutZero;
+
+  if (cWithoutZero == int(removeZero(c))) cout << "YES";
+  else cout << "NO";
+  return 0;
+}
